d72b2c258d282e631404560f0145a4d231bec474
Yash Shinde <111728261+Yashshinde43@users.noreply.github.com>
2025-02-07 19:44:50 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <Router>      
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp/>} /> 
         <Route path='/todo' element={<Todo />} />
         <Route path='/mail' element={<Mail />} />
